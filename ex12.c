74af55d5ce069e4692ca480d1dd2eb2021d6bae6
talesfragoso <talesfragoso@gmail.com>
2017-02-19 18:06:41 -0300

    Ex 12

---
+++ ex12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	//int areas[] = {10, 12, 13, 14, 20};
+	//char
+
+	char var;
+
+	scanf("%d", &var);
+	printf("%d", var);
+	return 0;
+}
+
