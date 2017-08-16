ecbbc8ead6ea064d890c25cb7415af5213fdeed2
talesfragoso <talesfragoso@gmail.com>
2017-08-16 01:46:09 -0300

    Ex 13_2

--- ex13_2.c
+++ ex13_2.c
@@ -3,6 +3,33 @@
 int main(int argc, char *argv[])
 {
 	short unsigned int i;
+	char states[4][30];
+	char *cities[] = {"Rio de Janeiro", "Niterói", "Curitiba", "Porto Alegre"};
 
-	char *states[30];
+	for(i = 1; i < argc; i++)
+	{
+		printf("%s\t", argv[i]);
+	}
+	puts(" ");
+	for(i = 0; i < 4; i++)
+	{
+		printf("Estate %d: ", i + 1);		
+		scanf("%s", states[i]);
+	}
+	puts(" ");	
+	for(i = 0; i < 4; i++)
+	{
+		printf("%s\t", states[i]);		
+		
+	}
+	printf("\n\nCities: " );
+	for(i = 0; i < 4; i++)
+	{
+		printf("%s\t", cities[i]);		
+		
+	}
+	puts(" ");
+	return 0;
+}
+	
 
