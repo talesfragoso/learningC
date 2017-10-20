0a140b95dd691c361ca005e947c065098cbeff26
talesfragoso <talesfragoso@gmail.com>
2017-10-20 04:07:10 -0200

    seguindo

---
+++ ex15.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	int ages[] = {23, 43, 12, 89};
+	char *names[] = {"Alan", "Franck", "Mary", "John", "Lisa"};
+	int *cur_age;
+	char **cur_name;
+	unsigned int cont, i;
+
+	
+	cont = sizeof(ages) / sizeof(int);
+	for
