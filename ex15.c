5f4bcd1a8dfd45609a90164da831c64801daf7cd
talesfragoso <talesfragoso@gmail.com>
2017-10-26 02:43:59 -0200

    Ex15 resumed and started Ex16.

--- ex15.c
+++ ex15.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int main(int argc, char *argv[])
 {
 	int ages[] = {23, 43, 12, 89};
@@ -9,4 +10,15 @@ int main(int argc, char *argv[])
 
 	
 	cont = sizeof(ages) / sizeof(int);
-	for
+	for(i = 0; i < cont; i++)
+	{
+		printf("Name: %s, Age: %d\n", names[i], ages[i]);
+	}
+	cur_age = ages;
+	cur_name = names;	
+	for(i = 0; i < cont; i++)
+	{
+		printf("Name: %s, Age: %d\n", *(cur_name + i), *(cur_age + i));
+	}
+	return 0;
+}
