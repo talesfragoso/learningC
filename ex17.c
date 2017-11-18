efa4e51b63bec70e9a8ac9082577ade24f62d717
talesfragoso <talesfragoso@gmail.com>
2017-11-18 03:31:30 -0200

    Starting with ex17

---
+++ ex17.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+
+#define MAX_DATA 512
+#define MAX_ROWS 100
+
+struct Address 
+{
+	int id;
+	int set;
+	char name[MAX_DATA];
+	char email[MAX_DATA];
+};
+
+struct Database
+{
+	struct Address rows[MAX_ROWS];
+};
+
+struct Connection
+{
+	FILE *file;
+	struct Database *db;
+};
+
+void die(const char *message);
+void Address_print(struct Address *addr);
+void Database_load(struct Connection *conn);
+struct Connection *Database_open(const char *filename, char mode);
+void Database_close(struct Connection *conn);
+void Database_write(struct Connection *conn);
+void Database_create(struct Connection *conn);
+void Database_set(struct Connection *conn, int id, const char *name, const char *email);
+void Database_get(struct Connection *conn, int id);
+void Database_delete(struct Connection *conn, int id);
+void Database_list(struct Connection *conn);	
