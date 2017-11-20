aec96ead85083f929d85bffcae167a7727a1d962
talesfragoso <talesfragoso@gmail.com>
2017-11-20 02:33:10 -0200

    Implemented 'main' in ex17

--- ex17.c
+++ ex17.c
@@ -43,7 +43,41 @@ int main(int argc, char *argv[])
 	char *filename;
 	char action;
 	struct Connection *conn;
-	int id;
+	int id = 0;
 
-	if (argc < 3) die("USAGE: ex17 <
-
+	if (argc < 3) die("USAGE: ex17 <dbfile> <action> [action params]");
+	filename = argv[1];
+	action = argv[2][0];
+	conn = Database_open(filename, action);
+	if (argc > 3) id = atoi(argv[3]);
+	if (id >= MAX_ROWS) die("There's not that many records");
+	switch (action)
+	{
+		case 'c': 
+			Database_create(conn);
+			Database_write(conn);
+			break;
+		case 'g':
+			if (argc != 4) die("Need an id to get");
+			Database_get(conn,id);
+			break;
+		case 's':
+			if (argc != 6) die("Need id, name, email to set");
+			Database_set(conn, argv[4], argv[5]);
+			Database_write(conn);
+			break;
+		case 'd':
+			if (argc != 4) die("Need an id to delete");
+			Database_delete(conn, id);
+			Database_write(conn);
+			break;
+		case 'l':
+			Database_list(conn);
+			break;
+		default:
+			die("Invalid action: c=create, g=get, s=set, d=del, l=list");
+	}
+	Database_close(conn);
+	return 0;
+}
+			
